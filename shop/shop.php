6a1b1204b2b419f05ba6eba5dcceea416124662e
waelvilla <waelvilla@gmail.com>
2018-12-05 17:17:51 +0300

    added shop.php

---
+++ shop/shop.php
@@ -0,0 +1,13 @@
+<?php include 'header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Watches Shop</title>
+</head>
+<body>
+    <div>hello</div>
+</body>
+
+
+<?php include 'footer.php'; ?>
