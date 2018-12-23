183a26bd1b90b2e0ac4a5736065751d71432dd47
waelvilla <waelvilla@gmail.com>
2018-12-23 21:57:00 +0300

    db

--- working/db.php
+++ working/db.php
@@ -3,8 +3,8 @@ $db=new Mysqli;
 
 $db->connect('sql141.main-hosting.eu.','u993407166_vypu','U4XnZhwLbeNW','u993407166_vypu');
 
-if($db){
-	echo 'success';
-}
+// if($db){
+// 	echo 'success';
+// }
 
 ?>
